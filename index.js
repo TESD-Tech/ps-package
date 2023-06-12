48b4c645968b083ce8d77cc686668a722bbaa4b9
Benjamin Kemp <bjkemp@gmail.com>
2023-06-12 12:59:39 -0400

    Add pagecataloging

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const archive_directory = 'plugin_archive'
 const build_directory = 'dist'
 const schema_directory = 'schema'
 const src_directory = 'src'
-const psFolders = ['permissions_root', 'user_schema_root', 'queries_root', 'WEB_ROOT']
+const psFolders = ['permissions_root', 'user_schema_root', 'queries_root', 'WEB_ROOT', 'pagecataloging']
 
 const format = 'yy.mm.patch' // CalVer filename format
 const junkFiles = ['.DS_Store', 'Thumbs.db', 'robots.txt', 'sitemap.xml', 'ssr-manifest.json']
