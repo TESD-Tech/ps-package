500cdbfa36487876ff367235b47eeb0c7dbd4e3d
Benjamin Kemp <bjkemp@gmail.com>
2023-08-10 10:43:39 -0400

    Add svelte switch

--- index.js
+++ index.js
@@ -1,38 +1,5 @@
-// This script parses an xml file 'ps-plugin.xml', converts it to an object,
-// writes the object back to the xml file, and then creates a zip file
-// containing the contents of the build_directory directory.
-//
-// Author: OpenAI & Benjamin Kemp
-// Date: 2022-12-16
-
-/*
-
-  Manual additions:
-  package.json version update
-  rename zip file using package name and version
-
-  Prompt:
-
-  Write a node.js script using es modules to:
-
-  1. Parse an xml file 'ps-plugin.xml' using xml2js into an object named psXML
-  2. Print psXML using console.dir
-  3. Write psXML as an xml file to ps-plugin.xml and 'dist/plugin.xml'
-  4. Create a new zip file named ps-plugin.zip containing the contents of the dist directory, including all subdirectories, using the archive library
-
-  Please be sure to:
-    1. use error handling
-    2. include comments
-    3. include a header in a comment block including author and date
-    4. include a comment block containing this prompt
-
-
-  --source <source>  The code source directory. Defaults to 'dist' (Vite)
-
-*/
-
-import path from 'path'
-import fs from 'fs'
+import path from 'node:path'
+import fs from 'node:fs'
 import xml2js from 'xml2js'
 import archiver from 'archiver'
 import calver from 'calver'
@@ -40,11 +7,12 @@ import minimist from 'minimist'
 
 const argv = minimist(process.argv.slice(2))
 const source = argv.source || 'src'
+const type = argv.type || 'vue'
 
-const archive_directory = 'plugin_archive'
-const build_directory = 'dist'
-const schema_directory = 'schema'
-const src_directory = source
+const archiveDirectory = 'plugin_archive'
+const buildDirectory = 'dist'
+const schemaDirectory = 'schema'
+const srcDirectory = source
 const psFolders = ['permissions_root', 'user_schema_root', 'queries_root', 'WEB_ROOT', 'pagecataloging']
 
 const format = 'yy.mm.patch' // CalVer filename format
@@ -52,221 +20,165 @@ const junkFiles = ['.DS_Store', 'Thumbs.db', 'robots.txt', 'sitemap.xml', 'ssr-m
 let zipFileName
 let schemaZipFileName
 
-// Create destination directories
-for (const dir of [archive_directory, build_directory, schema_directory]) {
-  if (!fs.existsSync(dir))
-    fs.mkdirSync(dir)
-}
+async function removeJunk(dir) {
+  const files = await fs.promises.readdir(dir)
+  for (const file of files) {
+    const fullPath = path.join(dir, file)
+    const stat = await fs.promises.stat(fullPath)
 
-const removeJunk = async (dir) => {
-  return new Promise((resolve, reject) => {
-    fs.readdir(dir, (err, files) => {
-      if (err) {
-        console.log(err)
-        return
-      }
-      files.forEach((file) => {
-        const fullPath = path.join(dir, file)
-
-        fs.stat(fullPath, (err, stat) => {
-          if (err) {
-            console.log(err)
-            return
-          }
-
-          if (stat.isDirectory()) {
-            removeJunk(fullPath)
-          }
-          else if (junkFiles.includes(file)) {
-            fs.unlink(fullPath, (err) => {
-              if (err)
-                console.log(err)
-
-              else
-                console.log(`Deleted junk file: ${fullPath}`)
-            })
-          }
-        })
-      })
-
-      resolve()
-    })
-  })
-}
-
-const mergePSfolders = async (dir) => {
-  for (const folder of psFolders) {
-    let this_dir = dir
-    if (folder === 'user_schema_root')
-      this_dir = schema_directory
-
-    if (fs.existsSync(`${this_dir}/${folder}`) && folder !== 'WEB_ROOT') {
-      // Clear out everything except WEB_ROOT
-      await new Promise((resolve, reject) => {
-        fs.rm(`${this_dir}/${folder}`, { recursive: true }, (err) => {
-          if (err) {
-            console.error(err)
-            reject(err)
-          }
-          else {
-            resolve()
-          }
-        })
-      })
-    }
-    if (fs.existsSync(`${src_directory}/powerschool/${folder}`)) {
-      await fs.cpSync(`${src_directory}/powerschool/${folder}`, `${this_dir}/${folder}`, { recursive: true })
-      console.log(`Merging ${src_directory}/powerschool/${folder} into ${this_dir}/${folder}`)
+    if (stat.isDirectory()) {
+      await removeJunk(fullPath)
+    } else if (junkFiles.includes(file)) {
+      await fs.promises.unlink(fullPath)
+      console.log(`Deleted junk file: ${fullPath}`)
     }
   }
 }
 
-const createPlugins = async (folders) => {
-  for (const folder of folders) {
-    let thisZip = zipFileName
+async function mergePSfolders(targetDir) {
+  for (const folder of psFolders) {
+    const thisDir = folder === 'user_schema_root' ? schemaDirectory : targetDir
 
-    if (folder === schema_directory)
-      thisZip = schemaZipFileName
+    if (fs.existsSync(`${thisDir}/${folder}`) && folder !== 'WEB_ROOT') {
+      await fs.promises.rm(`${thisDir}/${folder}`, { recursive: true })
+    }
 
-    if (fs.existsSync(`${folder}`)) {
-      await new Promise((resolve, reject) => {
-        // Create a zip file containing the contents of build_directory and all subdirectories
-        const output = fs.createWriteStream(`${archive_directory}/${thisZip}`)
-        const archive = archiver('zip', { zlib: { level: 9 } })
+    if (fs.existsSync(`${srcDirectory}/powerschool/${folder}`)) {
+      await fs.promises.cp(`${srcDirectory}/powerschool/${folder}`, `${thisDir}/${folder}`, { recursive: true })
+      console.log(`Merging ${srcDirectory}/powerschool/${folder} into ${thisDir}/${folder}`)
+    }
+  }
+}
 
-        output.on('close', () => {
-          console.log(`${thisZip} created. Size: ${archive.pointer()} Bytes`)
-        })
+async function createPluginZip(folder, zipFileName) {
+  if (fs.existsSync(folder)) {
+    return new Promise((resolve) => {
+      const output = fs.createWriteStream(`${archiveDirectory}/${zipFileName}`)
+      const archive = archiver('zip', { zlib: { level: 9 } })
 
-        archive.pipe(output)
-        archive.directory(`${folder}/`, false)
-        archive.finalize()
+      output.on('close', () => {
+        console.log(`${zipFileName} created. Size: ${archive.pointer()} Bytes`)
         resolve()
       })
-    }
+
+      archive.pipe(output)
+      archive.directory(`${folder}/`, false)
+      archive.finalize()
+    })
   }
 }
 
-const updatePackageJson = (v) => {
+async function updatePackageVersion(v) {
   try {
-    // Read the contents of 'package.json'
-    const packageJsonString = fs.readFileSync('package.json', 'utf8')
-
-    // Parse the 'package.json' string into an object
+    const packageJsonString = await fs.promises.readFile('package.json', 'utf8')
     const packageJson = JSON.parse(packageJsonString)
-
-    // Update the 'version' field with the value of 'v'
     packageJson.version = v
-
-    // Write the updated 'package.json' object back to the file
-    fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2))
-  }
-  catch (error) {
-    // Handle any errors that may have occurred
+    await fs.promises.writeFile('package.json', JSON.stringify(packageJson, null, 2))
+  } catch (error) {
     console.error(error)
   }
 }
 
-const pruneArchive = async () => {
-  // Prune ./plugin_archive and keep four most recent files
-  return new Promise((resolve) => {
-    fs.readdir(archive_directory, (err, files) => {
-      if (err) {
-        console.error(`Error reading directory: ${err}`)
-        return
-      }
+async function pruneArchive() {
+  try {
+    const files = await fs.promises.readdir(archiveDirectory);
+    files.sort((a, b) => {
+      const aTime = fs.statSync(path.join(archiveDirectory, a)).mtimeMs;
+      const bTime = fs.statSync(path.join(archiveDirectory, b)).mtimeMs;
+      return bTime - aTime;
+    });
 
-      files.sort((a, b) => {
-        const aTime = fs.statSync(path.join(archive_directory, a)).mtimeMs
-        const bTime = fs.statSync(path.join(archive_directory, b)).mtimeMs
-        return bTime - aTime
-      })
-
-      const recentFiles = files.slice(0, 3)
-
-      files.forEach((file) => {
-        if (!recentFiles.includes(file)) {
-          fs.unlink(path.join(archive_directory, file), (err) => {
-            if (err)
-              console.error(`Error deleting "${file}": ${err}`)
-          })
+    const recentFiles = files.slice(0, 3);
+    for (const file of files) {
+      if (!recentFiles.includes(file)) {
+        try {
+          await fs.promises.unlink(path.join(archiveDirectory, file));
+        } catch (error) {
+          console.error(`Error deleting "${file}": ${error}`);
         }
-      })
-    })
-
-    resolve()
-  })
+      }
+    }
+  } catch (error) {
+    console.error(`Error pruning archive: ${error}`);
+  }
 }
 
-const main = async () => {
-  try {
-    // Parse the xml file 'ps-plugin.xml' using xml2js and store the result in 'psXML'
-    const xml = fs.readFileSync('plugin.xml', 'utf8')
-    const psXML = await xml2js.parseStringPromise(xml)
+async function parseXml() {
+  const xml = await fs.promises.readFile('plugin.xml', 'utf8')
+  return await xml2js.parseStringPromise(xml)
+}
 
-    // TODO: Need to refactor
-    const packageJsonString = fs.readFileSync('package.json', 'utf8')
-    // Parse the 'package.json' string into an object
-    const packageJson = JSON.parse(packageJsonString)
+async function writeXml(psXML) {
+  const builder = new xml2js.Builder()
+  const xmlOutput = builder.buildObject(psXML)
 
-    const logErr = (err) => {
-      console.error(err)
-      errCount += 1
-    }
+  fs.writeFileSync('plugin.xml', xmlOutput)
+  fs.writeFileSync(`${buildDirectory}/plugin.xml`, xmlOutput)
 
-    // console.dir(psXML)
-
-    // Update version in ps-plugin.xml, dist/plugin.xml, and package.json
-    let newVersion
-    try {
-      newVersion = calver.inc(format, packageJson.version, 'calendar.patch')
-    }
-    catch (error) {
-      newVersion = calver.inc(format, '', 'calendar.patch')
-    }
-
-    psXML.plugin.$.version = newVersion
-    updatePackageJson(newVersion)
-
-    // Prep dist and dist-data for plugin creation
-    await mergePSfolders(build_directory)
-    await removeJunk(build_directory)
-
-    zipFileName = `${psXML.plugin.$.name.replaceAll(' ', '_')}-${newVersion}.zip`
-    zipFileName = zipFileName.replace('_-_', '-')
-    schemaZipFileName = `DATA-${zipFileName}`
-
-    const builder = new xml2js.Builder()
-    const xmlOutput = builder.buildObject(psXML)
-    fs.writeFileSync('plugin.xml', xmlOutput)
-
-    fs.writeFileSync(`${build_directory}/plugin.xml`, xmlOutput)
-
-    if (fs.existsSync(`${schema_directory}`)) {
-      // Create separate plugin.xml for data files
-      // Helps with high import lag in PS when user_schema_root is present
-      psXML.plugin.$.name += ' DATA'
-      delete psXML.plugin.access_request
-      const xmlOutput_DATA = builder.buildObject(psXML)
-      fs.writeFileSync(`${schema_directory}/plugin.xml`, xmlOutput_DATA)
-    }
-
-    // Remove root index.html
-    if (fs.existsSync(`${build_directory}/WEB_ROOT/index.html`)) {
-      fs.unlink(`${build_directory}/WEB_ROOT/index.html`, (err) => {
-        if (err)
-          console.error(err)
-
-        else
-          console.log('Deleted /index.html')
-      })
-    }
-
-    await createPlugins([build_directory, schema_directory])
-
-    await pruneArchive()
+  if (fs.existsSync(`${schemaDirectory}`)) {
+    psXML.plugin.$.name += ' DATA'
+    delete psXML.plugin.access_request
+    const xmlOutputData = builder.buildObject(psXML)
+    fs.writeFileSync(`${schemaDirectory}/plugin.xml`, xmlOutputData)
   }
-  catch (error) {
+}
+
+async function updateVersion() {
+  const packageJsonString = await fs.promises.readFile('package.json', 'utf8')
+  const packageJson = JSON.parse(packageJsonString)
+
+  let newVersion
+  try {
+    newVersion = calver.inc(format, packageJson.version, 'calendar.patch')
+  } catch (error) {
+    newVersion = calver.inc(format, '', 'calendar.patch')
+  }
+
+  const psXML = await parseXml()
+  psXML.plugin.$.version = newVersion
+  await updatePackageVersion(newVersion)
+  await writeXml(psXML)
+}
+
+async function prepareBuildDirectory() {
+  await mergePSfolders(buildDirectory)
+  await removeJunk(buildDirectory)
+
+  if (fs.existsSync(`${buildDirectory}/WEB_ROOT/index.html`)) {
+    await fs.promises.unlink(`${buildDirectory}/WEB_ROOT/index.html`)
+    console.log('Deleted /index.html')
+  }
+}
+
+async function createZipFiles(psXML, newVersion) {
+  zipFileName = `${psXML.plugin.$.name.replaceAll(' ', '_')}-${newVersion}.zip`.replace('_-_', '-')
+  schemaZipFileName = `DATA-${zipFileName}`
+
+  await createPluginZip(buildDirectory, zipFileName)
+  await createPluginZip(schemaDirectory, schemaZipFileName)
+}
+
+async function copySvelteBuildContents(pluginName) {
+  if (type === 'svelte' && fs.existsSync('./public/build/')) {
+    const targetDir = `./dist/WEB_ROOT/${pluginName}`
+    await ncp('./public/build/', targetDir)
+    console.log(`Copied Svelte build contents to ${targetDir}`)
+  }
+}
+
+async function main() {
+  try {
+    const psXML = await parseXml()
+    const packageJsonString = await fs.promises.readFile('package.json', 'utf8')
+    const packageJson = JSON.parse(packageJsonString)
+    const newVersion = await calver.inc(format, packageJson.version, 'calendar.patch')
+
+    await updatePackageVersion(newVersion)
+    await prepareBuildDirectory()
+    await copySvelteBuildContents(psXML.plugin.$.name) // Pass the plugin name
+    await createZipFiles(psXML, newVersion)
+    await pruneArchive()
+  } catch (error) {
     console.error(error)
   }
 }
